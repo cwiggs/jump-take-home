3eea63757dd6d27c1642003f615ca5bec7ca9c6a
Chris Wiggins <me@cwiggs.com>
2025-10-22 14:38:07 -0600

    fix: looks like you can't expose the image info, exposing node and pod info instead

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 # Grab env var for image tag so we can pass into the app from k8s.
-image_tag = os.getenv("IMAGE_TAG", "default")
-image_digest = os.getenv("IMAGE_DIGEST", "default")
+node_name = os.getenv("NODE_NAME", "default")
+pod_ip = os.getenv("POD_IP", "default")
 
 # Create a route at the root URL that returns "Hello, World"
 @app.route("/")
 def hello():
-    return f"Hello World. I was updated with CI! Image Tag: {image_tag}, Image Digest: {image_digest}"
+    return f"Hello World. I was updated with CI! Running on node: {node_name}, pod IP: {pod_ip}\n"
 
 if __name__ == "__main__":
     # Run the application on all interfaces/IPs.
