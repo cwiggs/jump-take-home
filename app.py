c0706869d0266d107251c4ba423e970b44c4df80
Chris Wiggins <me@cwiggs.com>
2025-10-22 14:28:49 -0600

    feat: add ability to add env vars to output of app

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 # Grab env var for image tag so we can pass into the app from k8s.
 image_tag = os.getenv("IMAGE_TAG", "default")
+image_digest = os.getenv("IMAGE_DIGEST", "default")
 
 # Create a route at the root URL that returns "Hello, World"
 @app.route("/")
 def hello():
-    return "Hello World. I was updated with CI!"
+    return f"Hello World. I was updated with CI! Image Tag: {image_tag}, Image Digest: {image_digest}"
 
 if __name__ == "__main__":
     # Run the application on all interfaces/IPs.
