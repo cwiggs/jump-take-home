0c86d0681f9625656bd6ea9fced5feb93c13a7ab
Chris Wiggins <me@cwiggs.com>
2025-10-22 14:02:23 -0600

    feat: add basic py/flask app with container img

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Grab env var for image tag so we can pass into the app from k8s.
+image_tag = os.getenv("IMAGE_TAG", "default")
+
+# Create a route at the root URL that returns "Hello, World"
+@app.route("/")
+def hello():
+    return "Hello World"
+
+if __name__ == "__main__":
+    # Run the application on all interfaces/IPs.
+    app.run(host="0.0.0.0")
